5eafa8567f2fa81d3615101d37251b830fef3ddd
ehab qino <ehabqino@gmail.com>
2021-07-01 17:28:48 +0300

    commit

--- src/js/appController.js
+++ src/js/appController.js
@@ -1,11 +1,25 @@
-require(['ojs/ojcore','knockout','ojs/ojbootstrap','jquery','ojs/ojmodule-element-utils','ojs/ojmodule-element','ojs/ojbutton'],
-    function(oj,ko,bs,$,moduleUtils){
+require(['ojs/ojcore','knockout','ojs/ojbootstrap','jquery','ojs/ojcorerouter','ojs/ojurlparamadapter',
+         'ojs/ojarraydataprovider',
+         'ojs/ojmodule-element-utils','ojs/ojmodule-element','ojs/ojbutton'],
+    function(oj,ko,bs,$,CoreRouter,UrlParamAdapter,ArrayDataprovider,moduleUtils){
         class SPAModule {
             constructor(){
                 //Define Routes
-                this.routes = [{path:'page1', detail:{label:'First Page'}},
+                this.routes = [{path:"", redirect:"page2"},
+                               {path:'page1', detail:{label:'First Page'}},
                                {path:'page2', detail:{label:'Second Page'}},
-                               {path:'page3', detail:{label:'Third Page'}},]
+                               {path:'page3', detail:{label:'Third Page'}},];
+                this.router = new CoreRouter(this.routes,{
+                    urlAdapter : new UrlParamAdapter()
+                });
+                this.dataProvider = new ArrayDataprovider(this.routes.slice(1),{
+                    KeyAttributes:"path"
+                });
+
+                this.module = new ModuleRouterAdapter(this.routes,{
+                    viewPath : 'views/',
+                    viewModelPath : 'viewModels/'
+                });
                 
             }//end constructor
 
