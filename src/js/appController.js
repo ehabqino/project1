eda28102288fc63b79af67f502c51c2a880fa82a
ehab qino <ehabqino@gmail.com>
2021-07-01 21:01:44 +0300

    commit

--- src/js/appController.js
+++ src/js/appController.js
@@ -1,7 +1,7 @@
 require(['ojs/ojcore','knockout','ojs/ojbootstrap','jquery','ojs/ojcorerouter','ojs/ojurlparamadapter',
          'ojs/ojarraydataprovider','ojs/ojmodulerouter-adapter',
          'ojs/ojknockoutrouteradapter',
-         'ojs/ojmodule-element-utils','ojs/ojmodule-element','ojs/ojbutton'],
+         'ojs/ojmodule-element-utils','ojs/ojnavigationlist','ojs/ojmodule-element','ojs/ojbutton'],
     function(oj,ko,bs,$,CoreRouter,UrlParamAdapter,ArrayDataprovider,
         ModuleRouterAdapter,KnockoutRouterAdapter,moduleUtils){
         class SPAModule {
@@ -15,10 +15,10 @@ require(['ojs/ojcore','knockout','ojs/ojbootstrap','jquery','ojs/ojcorerouter','
                     urlAdapter : new UrlParamAdapter()
                 });
                 this.dataProvider = new ArrayDataprovider(this.routes.slice(1),{
-                    KeyAttributes:"path"
+                    keyAttributes:"path"
                 });
 
-                this.module = new ModuleRouterAdapter(this.routes,{
+                this.module = new ModuleRouterAdapter(this.router,{
                     viewPath : 'views/',
                     viewModelPath : 'viewModels/'
                 });
